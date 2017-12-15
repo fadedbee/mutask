f6333e790112b7eae2fbf17ccfecd5153e949695
fadedbee <github@fadedbee.com>
2017-12-15 18:35:41 +0000

    started work...

--- routes/index.js
+++ routes/index.js
@@ -4,25 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/session/' + new_id());
-});
-
-router.get('/session/:sid', function(req, res, next) {
-  res.render('index', { title: 'Express', sid: req.params.sid });
+  res.render('index', { title: 'muTask' });
 });
 
 module.exports = router;
 
-var crypto = require('crypto');
-
-function new_id() {
-  var id = '';
-  while (id.length < 8) {
-    var num = 62;
-    while (num > 61) {
-      num = crypto.randomBytes(1)[0];
-    }
-    id += "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"[num];
-  }
-  return id;
-}
