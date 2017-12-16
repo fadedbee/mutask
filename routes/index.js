22451cf9f837afdb9006ea53e0839ba3b9b58173
fadedbee <github@fadedbee.com>
2017-12-16 15:17:33 +0000

    added bootstrap and email sending

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,39 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
+
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'muTask' });
+});
+
+router.post('/login', function(req, res, next) {
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_FROM,
+    to: process.env.EMAIL_TO,
+    subject: 'test',
+    html: '<p>Your html here</p>',
+    text: 'your text here'
+  };
+
+  transporter.sendMail(mailOptions, function (err, info) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.info(info);
+    }
+  });
 });
 
 module.exports = router;
